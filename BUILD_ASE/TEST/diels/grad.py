688b19da4d61e66fad240f62b2d2abc4f26152f7
Soumik Das <soumikd@umich.edu>
2025-04-25 16:46:10 -0400

    updated grad.py to properly use ASE-based QChem calculator

--- BUILD_ASE/TEST/diels/grad.py
+++ BUILD_ASE/TEST/diels/grad.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
+#!/home/soumikd/.conda/envs/pycosanet/bin/python
 
+import shutil
 from ase import Atoms
 from ase.visualize import view
 from ase.calculators.emt import EMT
-from ase.calculators.nwchem import NWChem
+from ase.calculators.qchem import QChem
 from ase.constraints import FixAtoms
 from ase.io import write
 from ase.io import read
@@ -14,27 +15,43 @@ nargs = len(sys.argv)
 argv1 = sys.argv[1]
 argv2 = sys.argv[2]
 #print 'argv1: ',argv1
-fname = 'scratch/structure'+argv1
-slab = read(fname)
+os.system("cp scratch/structure"+argv1+" scratch/structure"+argv1+".xyz")
+fname = 'scratch/structure'+argv1+'.xyz'
+atoms = read(fname)
+folder = 'scratch/qchem'+argv1
 
-#slab.set_calculator(EMT())
-slab.set_calculator(NWChem())
-slab.calc.label = 'scratch/nwchem'+argv1
-inputfile = 'nwchem'+argv1+'.nw'
-outputfile = 'nwchem'+argv1+'.out'
-##slab.calc.command = 'cd scratch; mpirun -np '+argv2+' nwchem '+inputfile+' > '+outputfile
-slab.calc.command = 'cd scratch; mpirun --mca btl ^openib -np '+argv2+' nwchem '+inputfile+' > '+outputfile
-print 'NWChem command: ',slab.calc.command
-os.system('rm -f '+slab.calc.label+'.scrdir')
-os.system('ln -s $PBSTMPDIR '+slab.calc.label+'.scrdir')
+#atoms.set_calculator(EMT())
+calc = QChem(
+    jobtype='FORCE',
+    method='B3LYP',
+    basis='6-31G*',
+    nt=4,
+)
 
-slab.calc.parameters.xc = 'B3LYP'
-#slab.calc.parameters.basis = 'LANL2DZ ECP'
-#slab.calc.parameters.ecp = 'LANL2DZ ECP'
-slab.calc.reset()
+atoms.calc = calc
 
-energy = - slab.get_potential_energy()
-grads = - slab.get_forces()
+atoms.calc.label = 'scratch/qchem'+argv1
+inputfile = 'qchem'+argv1+'.inp'
+outputfile = 'qchem'+argv1+'.out'
+##atoms.calc.command = 'cd scratch; mpirun -np '+argv2+' nwchem '+inputfile+' > '+outputfile
+# atoms.calc.command = 'cd scratch; mpirun --mca btl ^openib -np '+argv2+' nwchem '+inputfile+' > '+outputfile
+atoms.calc.command = f'cd scratch; qchem -nt 4 '+inputfile+' > '+outputfile
+print('QChem command: ',atoms.calc.command)
+# os.system('ln -s $PBSTMPDIR '+atoms.calc.label+'.scrdir')
+
+# atoms.calc.parameters.xc = 'B3LYP'
+#atoms.calc.parameters.basis = 'LANL2DZ ECP'
+#atoms.calc.parameters.ecp = 'LANL2DZ ECP'
+atoms.calc.reset()
+
+cwd = os.getcwd()
+
+if not os.path.exists(folder):
+    os.mkdir(folder)
+os.chdir(folder)
+
+energy = - atoms.get_potential_energy()
+grads = - atoms.get_forces()
 #print grads
 f = open('scratch/GRAD'+argv1, 'w')
 f.write(str(energy))
@@ -42,3 +59,10 @@ f.write('\n')
 f.write(str(grads))
 f.write('\n')
 f.close()
+
+shutil.copy2('scratch/GRAD'+argv1, cwd+'/scratch')
+os.chdir(cwd)
+
+os.system('rm -rf '+folder)
+os.system('rm -f scratch/structure'+argv1)
+os.system('rm -f '+fname)
